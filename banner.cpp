84c0ffe1d30f157735caf69fe0ad3b0f2be5fd74
Saad@DESKTOP-UBGD9AO <saadakmal460@gmail.comgit config --global user.email saadakmal460@gmail.com>
2022-12-16 23:18:26 +0500

    PD 2 codes

---
+++ banner.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+
+main(){
+system("Color 0F");
+
+
+cout << "              %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%          "     <<endl;
+cout << "              %                                                             %           " <<endl;
+cout << "              %                                                             %          " <<endl;
+cout << "              %                                                             %           " <<endl;
+cout << "              %                                                             %        " <<endl;
+cout << "              %                 HOSTEL MANAGMENT SYSYTEM                    %         " <<endl;
+cout << "              %                                                             %           " <<endl;
+cout << "              %                                                             %           " <<endl;
+cout << "              %                                                             %           " <<endl;
+cout << "              %                                                             %           " <<endl;
+cout << "              %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%          "     <<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+}
---
+++ cow.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+
+main(){
+
+system("Color 8F");
+
+cout << "           -----------------------------------------           " <<endl;
+cout<< endl;
+cout << "                        o      ^__^                           " <<endl;
+cout << "                         o     (oo)\\________                   " <<endl;      
+cout << "                               (__)\\        )\\/\\             " <<endl;
+cout << "                                    ||-----w  |                 " <<endl;
+cout << "                                    ||       ||                 " <<endl;                                     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
